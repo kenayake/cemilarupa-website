87b314e4d84920953b5bd051638be3f651c7b4d9
Kenny Ekanayake <kenny.ekanayake@gmail.com>
2024-03-15 23:20:33 +0700

    tentang kami

--- src/routes/home/about.svelte
+++ src/routes/home/about.svelte
@@ -1,8 +1,25 @@
-<section class="font-londrina bg-primary text-tertiary" id="tentang-kami">
-    <h1>TENTANG KAMI</h1>
-    <p>Cemilarupa merupakan produk UMKM yang berasal dari Desa Pajambon, Kuningan. Keripik Cemilarupa dibuat sendiri dengan rasa yang . Bumbu yang digunakan merupakan Bumbu ciri khas Indonesia. Dengan begitu, produk yang ditawarkan kepada konsumen adalah abon asli Indonesia. </p>
+<script>
+	import ibuPKK from '$lib/images/about/ibu_pkk.jpg';
+</script>
 
-        <p>BringBon memiliki 3 varian, yaitu sapi, ayam, dan ikan. Daging yang digunakan berkualitas tinggi dan memanfaatkan produk lokal. Seiring berjalannya waktu, BringBon kini menyediakan varian abon dengan tingkat kepedasan. Dimulai dari original, level pedas selow, pedas mantul, dan pedas doweeer.</p>
-        
-        <p>BringBon menawarkan tiga ukuran yang tersedia untuk dibeli konsumen. Dimulai dari 50 gram, 100 gram, dan 250 gram. BringBon menggunakan packaging berupa toples sehingga keamanan terjaga. BringBon juga menawarkan produk bundling yang berisi ketiga varian BringBon.</p>
+<section
+	class="font-londrina bg-primary text-tertiary py-10 px-2 lg:px-40 lg:text-xl"
+	id="tentang-kami"
+>
+	<h1 class="text-center text-5xl mb-10">TENTANG KAMI</h1>
+	<div class="mb-10 lg:w-[40vw] lg:mx-auto"><img src={ibuPKK} alt="gambar ibu-ibu PKK desa pajambon" class="rounded-3xl" /></div>
+	<div class="space-y-5 lg:space-y-10 text-justify">
+		<p class="">
+			Tim Pembinaan Kesejahteraan Keluarga (PKK) Desa Pajambon bekerja di bawah administrasi Desa
+			Pajambon dan berfokus pada pemberdayaan keluarga dan meningkatkan kesejahteraan mereka. PKK
+			Desa Pajambon berkonsentrasi pada bidang-bidang seperti: Pendataan bayi balita dan ibu hamil,
+			kelas ibu hamil, dan kegiatan rutin bulanan seperti Posyandu, Posbindu, dan Posrem.
+		</p>
+		<p>
+			Pada bulan Februari 2023, tim PKK Desa Pajambon menerima bantuan berupa sembako. Kemudian,
+			atas inisiatif ketua PKK, Ibu Selpiyanti, bantuan tersebut sebagian dijual dan dijadikan modal
+			untuk memulai usaha kecil di PKK. Usaha tersebut bergerak di bidang pembuatan cemilan yang
+			diberi nama "Cemilarupa".
+		</p>
+	</div>
 </section>
