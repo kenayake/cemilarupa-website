b98e8130bd4b0ddd930a1321e980afed0da8403c
Kenny Ekanayake <23502110001@prasetiyamulya.ac.id>
2024-03-15 14:23:05 +0700

    SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,16 @@
 	import '@fontsource/darumadrop-one';
 	import '@fontsource/barlow-condensed/400.css';
 	import '@fontsource/barlow-condensed/700.css';
-    import '@fontsource/londrina-solid';
+	import '@fontsource/londrina-solid';
 </script>
 
+<svelte:head>
+	<title>Cemilarupa: Cemilan Enak Urang Pajambon</title>
+	<meta
+		name="description"
+		content="Menjual cemilan-cemilan khas pajambon seperti basreng (bakso goreng), bolring (bolet kering), molring (cimol kering), kripang (kripik pangsit) dan siebol (sistik enak boled), dengan bumbu khas yang enak dan lezat."
+	/>
+</svelte:head>
 <div class="w-screen">
 	<Nav />
 	<slot />
